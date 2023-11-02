807519e3e9e5a0cfabd9c84bc18becd41b2e6400
arete989 <arete989@gmail.com>
2023-11-01 17:24:51 -0700

    added more unit tests. all views so far are now covered.

--- tarot/api/tests.py
+++ tarot/api/tests.py
@@ -44,3 +44,42 @@ class TestGetCards(TestCase):
             },
         ])
 
+class TestGetCard(TestCase):
+
+    def setUp(self):
+        self.card1 = Card(
+            title='Queen of Swords',
+            explanation='archetype of an older, wiser feminine intellect',
+        )
+        self.card1.save()
+        self.card2 = Card(
+            title='Queen of Pentacles',
+            explanation='down to earth, practical, thrify',
+        )
+        self.card2.save()
+
+    def test_get_card_success_case(self):
+        request = Mock()
+        card_id = self.card2.id
+
+        response = GetCard().get(request, card_id)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {
+            'id': self.card2.id,
+            'title': self.card2.title,
+            'explanation': self.card2.explanation,
+
+        })
+
+    def test_get_card_with_invalid_card_id(self):
+        request = Mock()
+        card_id = self.card2.id + 1
+
+        with self.assertRaises(ValidationError) as e:
+            response = GetCard().get(request, card_id)
+
+        self.assertEqual(
+            e.exception.args[0],
+            'The card you requested does not exist',
+        )
