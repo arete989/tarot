de54c460164e13c8dfe35adadab07603ca06b25a
arete989 <arete989@gmail.com>
2023-11-01 17:16:45 -0700

    add first test case. ran test suite and it works! yay.

--- README.md
+++ README.md
@@ -16,11 +16,19 @@ DAY 3 (1 hour)
 * Achieved! Only took an hour :)
 * And also solved some minor bugs
 
-FUTURE
-* Write more views & models
+DAY 4 (2 hours)
+
+* Refactor to class-based views per the Django REST Framework tutorial (why didn't they just say that from the beginning...)
+* Manually retest and everything works - same place as I was on Day 3
+* Annoyed with manual testing - this is good time to write a unit test suite. So I did.
+
+NEXT
+* TODO: Add models for Reading, Image, User
+* TODO: Write views for modifying the above
+* TODO: Add user auth as I'm doing the above - if it makes sense
 * Goal: Get all of the API endpoints working locally
 * Goal: Write test suite to cover all the API endpoints
-* Goal: Deploy and able to call API endpoints
+* Goal: Deploy and able to call API endpoints from non-local server
 
 
 
--- tarot/api/tests.py
+++ tarot/api/tests.py
@@ -1,3 +1,46 @@
+from unittest.mock import Mock
+
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.exceptions import ValidationError
+
+from api.models import Card
+from api.views import (
+    GetCard,
+    GetCards,
+)
+
+
+class TestGetCards(TestCase):
+
+    def setUp(self):
+        self.card1 = Card(
+            title='Queen of Swords',
+            explanation='archetype of an older, wiser feminine intellect',
+        )
+        self.card1.save()
+        self.card2 = Card(
+            title='Queen of Pentacles',
+            explanation='down to earth, practical, thrify',
+        )
+        self.card2.save()
+
+    def test_get_cards_success_case(self):
+        request = Mock()
+        response = GetCards().get(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [
+            {
+                'id': self.card1.id,
+                'title': self.card1.title,
+                'explanation': self.card1.explanation,
+            },
+            {
+                'id': self.card2.id,
+                'title': self.card2.title,
+                'explanation': self.card2.explanation,
+
+            },
+        ])
+
